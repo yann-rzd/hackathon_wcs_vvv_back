bcbe1b45f5e1d8cc15611485111d809759a47360
Fredy <fredy.ly.wcs@gmail.com>
2021-01-15 10:51:00 +0100

    adding slug in get route

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.get('/product', async (req, res) => {
   console.log('object')
   try {
-    const select = await connection.query('SELECT pdt.id, pdt.name, pdt.price, pdt.description, pdt.image, pdt.quantity, pdt.duration_effect, pwr.power, afEff.effect FROM product pdt JOIN power pwr ON pwr.id = pdt.power_id JOIN after_effect afEff ON afEff.id = pdt.after_effect_id');
+    const select = await connection.query('SELECT pdt.slug, pdt.id, pdt.name, pdt.price, pdt.description, pdt.image, pdt.quantity, pdt.duration_effect, pwr.power, afEff.effect FROM product pdt JOIN power pwr ON pwr.id = pdt.power_id JOIN after_effect afEff ON afEff.id = pdt.after_effect_id');
     res.status(200).json(select[0]);
   }
   catch (err) {
